2b8bdf36998e8f2d74d35e487396022a07183da7
m-11398918 <115518093+m-11398918@users.noreply.github.com>
2022-10-18 11:19:40 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        pins.analogWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
+        pins.analogWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+    }
+})
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
 })
